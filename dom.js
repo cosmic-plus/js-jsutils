98eabac84fad7006abad14a1529010a8e04f7125
Antoine <Mister.Ticot@gmail.com>
2018-09-21 19:08:34 -0500

    Add 'dom.js' module

---
+++ dom.js
@@ -0,0 +1,28 @@
+'use_strict'
+/**
+ * A simple module that ease access to HTML DOM nodes. Any HTML element having
+ * an `id` at loading time is registered at dom[id]. This is to avoid running
+ * `querySelector` multiple times.
+ *
+ * @exports dom
+ */
+const dom = exports
+
+const html = require('./html')
+
+/**
+ * Main tags.
+ */
+dom.html = html.grab('html')
+dom.head = html.grab('head')
+dom.body = html.grab('body')
+dom.header = html.grab('header')
+dom.main = html.grab('main')
+dom.footer = html.grab('footer')
+
+/**
+ * All elements having an ID.
+ */
+document.querySelectorAll('[id]').forEach(element => {
+  dom[element.id] = element
+})
