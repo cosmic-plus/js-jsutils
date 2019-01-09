c2370079d236bd5dc73c01e142b503935ba4b071
MisterTicot <mister.ticot@cosmic.plus>
2019-01-09 15:50:36 +0100

    nice.js: various bugfixes

--- nice.js
+++ nice.js
@@ -7,17 +7,17 @@
  * @return {String} The formatted value
  */
 module.exports = function (value, precision) {
-  if (value === undefined) return "..."
-  else if (value === 0) return 0
+  if (value === undefined || isNaN(value)) return "..."
+  else if (value == 0) return 0
 
-  if (!precision) precision = precisionAuto(value)
+  if (precision == null) precision = precisionAuto(value)
   return Number(value).toFixed(precision)
 }
 
 
 function precisionAuto (value) {
   let precision = 0
-  const str = "" + value
+  const str = String(value)
   const firstNonZero = str.search(/[^0.]/)
   if (firstNonZero) {
     precision = firstNonZero + 2
@@ -28,5 +28,5 @@ function precisionAuto (value) {
   }
 
   if (precision) precision = Math.round(precision / 2) * 2
-  return precision
+  return precision > 0 ? precision : 0
 }
